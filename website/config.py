a16d832dd739088aaa7d3b31bd9c94783ad2ac37
krassowski <krassowski.michal@gmail.com>
2016-05-19 11:38:51 +0200

    Store the temporary database permanently
    so it does not require db_create after resteing /tmp

--- website/config.py
+++ website/config.py
@@ -1,4 +1,9 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+import os
+base_dir = os.path.abspath(os.path.dirname(__file__))
+databases_dir = os.path.join(base_dir, 'databases')
+
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(databases_dir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 # SQLALCHEMY_ECHO = False
 SECRET_KEY = '\xfb\x12\xdf\xa1@i\xd6>V\xc0\xbb\x8fp\x16#Z\x0b\x81\xeb\x16'
