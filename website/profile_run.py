164b386d97929816eaecbebe7a47164202acad8f
krassowski <krassowski.michal@gmail.com>
2016-06-22 21:15:13 +0200

    Add a script for profiling the app

---
+++ website/profile_run.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from werkzeug.contrib.profiler import ProfilerMiddleware
+from app import app
+
+app.config['PROFILE'] = True
+app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
+app.run(debug = True)
