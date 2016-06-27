a9079041c021eb2ed7baba8d73048fb765790ac6
krassowski <krassowski.michal@gmail.com>
2016-06-27 18:00:38 +0200

    Start groups collapsed, expand on click #7

--- website/static/network.js
+++ website/static/network.js
@@ -118,12 +118,16 @@ var Network = (function ()
             )
             kinase.node_id = i + 1
 
+
             // make links to the central protein's node from those
             // kinases that do not belong to any of groups
             if(kinases_in_groups.indexOf(kinase.name) === -1)
             {
                 addEdge(kinase.node_id, 0)
             }
+
+            // this property will be populated for kinases belonging to group in prepareKinaseGroups
+            kinase.group = undefined
         }
     }
 
@@ -132,6 +136,8 @@ var Network = (function ()
         for(var i = 0; i < kinase_groups.length; i++)
         {
             var group = kinase_groups[i]
+
+            group.is_group = true
 
             group.x = Math.random() * config.width
             group.y = Math.random() * config.height
@@ -143,9 +149,11 @@ var Network = (function ()
             for(var j = 0; j < group_kinases.length; j++)
             {
                 var kinase = group_kinases[j]
+                kinase.group = i
+
                 mutations_in_kinases += kinase.protein ? kinase.protein.mutations_count : 0
 
-                addEdge(group_index, kinase.node_id)
+                addEdge(kinase.node_id, group_index)
             }
 
             group.r = calculateRadius(
@@ -158,17 +166,63 @@ var Network = (function ()
         }
     }
 
+    function linkDistance(edge)
+    {
+        // make links between the core protein and groups longer,
+        // so the groups stand out and do not colide with kinases
+        if(edge.source.is_group)   // source node is a group
+        {
+            return 175
+        }
+        // adjust the length of a link between a kinase located in
+        // a group and its group's node
+        if(edge.target.is_group)    // target node is a group
+        {
+            return edge.target.expanded ? edge.target.r + edge.source.r : 0
+        }
+        return 100
+    }
+
+    function switchGroupState(node, state)
+    {
+        node.expanded = (state === undefined) ? !node.expanded : state
+
+        d3.selectAll('circle')
+            .filter(function(d)
+            {
+                return node.kinases.indexOf(d.name) != -1
+            })
+            .transition().ease('linear').duration(600)
+            .attr('r', function(d){return node.expanded ? d.r : 0})
+
+        d3.selectAll('.label')
+            .filter(function(d)
+            {
+                return node.kinases.indexOf(d.name) != -1
+            })
+            .transition().ease('linear').duration(600)
+            .attr('opacity', node.expanded ? 1 : 0)
+
+         d3.selectAll('.link')
+            .filter(function(e)
+            {
+                return node.kinases.indexOf(e.source.name) != -1
+            })
+            .transition().ease('linear').duration(600)
+            .attr('opacity', node.expanded ? 1 : 0)
+    }
+
+    function startsVisible(node)
+    {
+        // whether a node should start visible or not
+        // nodes belonging to groups should be hidden on start
+        return (node.group === undefined) ? 1 : 0
+    }
+
     var publicSpace = {
         init: function(user_config)
         {
             configure(user_config)
-
-            var force = d3.layout.force()
-                .gravity(0.05)
-                .distance(100)
-                .charge(-100)
-                .size([config.width, config.height])
-
 
             var vis = d3.select(config.element).append('svg')
                 .attr('preserveAspectRatio', 'xMinYMin meet')
@@ -190,51 +244,71 @@ var Network = (function ()
             prepareKinaseGroups(nodes_data.length)
             Array.prototype.push.apply(nodes_data, kinase_groups)
 
-            force
+            var force = d3.layout.force()
+                .gravity(0.05)
+                .distance(100)
+                .charge(-100)
+                .size([config.width, config.height])
                 .nodes(nodes_data)
                 .links(edges)
+                .linkDistance(linkDistance)
                 .start()
 
-            var link = vis.selectAll(".link")
+            var links = vis.selectAll(".link")
                 .data(edges)
                 .enter().append("line")
                 .attr("class", "link")
-                .style("stroke-width", function(d) { return Math.sqrt(d.weight); });
+                .attr('opacity', function(e){ return startsVisible(e.source) } )
+                .style("stroke-width", function(d) { return Math.sqrt(d.weight) })
 
             var nodes = vis.selectAll('.node') 
                 .data(nodes_data)
                 .enter().append('g')
-                .attr('transform', function(d){return 'translate(' + [d.x, d.y] + ')'})
+                .attr('transform', function(d){ return 'translate(' + [d.x, d.y] + ')' })
                 .attr('class', 'node')
                 .call(force.drag)
                 .on('click', function(node) {
                     if(d3.event.defaultPrevented === false)
                     {
-                        window.location.href = config.nodeURL(node)
+                        if(node.is_group)
+                        {
+                            switchGroupState(node)
+                        }
+                        else
+                        {
+                            window.location.href = config.nodeURL(node)
+                        }
                     }
                 })
 
-            nodes.append('circle')
-                .attr('class', 'nodes')
-                .attr('r', function(node){ return node.r })
+            var circles = nodes.append('circle')
+                .attr('r', function(d){ return startsVisible(d) ? d.r : 0})
                 .attr('stroke', function(node) {
                     var default_color = '#905590'
                     return node.color || default_color
                 }) 
 
-            nodes.append('text')
+            var labels = nodes.append('text')
+                .attr('class', 'label')
                 .text(function(d){return d.name})
                 .style('font-size', function(d) { return fitTextIntoCircle(d, this) })
                 .attr('dy', '.35em')
+                .attr('opacity', startsVisible)
                 
-            force.on('tick', function() {
-                link.attr("x1", function(d) { return d.source.x })
+            force.on('tick', function(e) {
+
+                links
+                    .attr("x1", function(d) { return d.source.x })
                     .attr("y1", function(d) { return d.source.y })
                     .attr("x2", function(d) { return d.target.x })
                     .attr("y2", function(d) { return d.target.y })
-                    nodes.attr('transform', function(d){return 'translate(' + [d.x, d.y] + ')'})
-                })
 
+                nodes.attr('transform', function(d){ return 'translate(' + [d.x, d.y] + ')'} )
+
+                force
+                    .linkDistance(linkDistance)
+                    .start()
+                })
         }
     }
 
