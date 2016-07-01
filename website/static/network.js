24b5f398f02798e46b45200dc70ddb1c3fe34c32
krassowski <krassowski.michal@gmail.com>
2016-07-01 21:23:41 +0200

    Remove example (explained in prev. commit message)

--- website/static/network.js
+++ website/static/network.js
@@ -241,7 +241,7 @@ var Network = (function ()
         // a kinase located in a group and its group's node
         if(edge.target.is_group)    // target node is a group
         {
-            return edge.target.expanded ? edge.target.orbits.getRadiusByNode(edge.source) : 0
+            return edge.target.expanded ? edge.target.r + edge.source.r : 0
         }
         return 100
     }
@@ -251,12 +251,6 @@ var Network = (function ()
         time = (time === undefined) ? 600 : time
         node.expanded = (state === undefined) ? !node.expanded : state
 
-        if(node.expanded)
-        {
-            node.orbits = Orbits()
-            node.orbits.init(getKinasesByName(node.kinases, kinases_grouped), node, {spacing:1, first_ring_scale:1})
-            node.orbits.placeNodes()
-        }
         function inGroup(d)
         {
             return node.index === d.group
