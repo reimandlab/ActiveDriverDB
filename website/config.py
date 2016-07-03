49730c758f9af19f4e17b46c50e40c6e5e6fce54
krassowski <krassowski.michal@gmail.com>
2016-07-04 00:24:38 +0200

    Update config template - mysql will be required
    everywhere with the following commit

--- website/config.py
+++ website/config.py
@@ -1,12 +1,6 @@
-import os
-base_dir = os.path.abspath(os.path.dirname(__file__))
-databases_dir = os.path.join(base_dir, 'databases')
-
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(databases_dir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'mysql://user:pass@localhost/db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-# SQLALCHEMY_ECHO = False
-SECRET_KEY = '\xfb\x12\xdf\xa1@i\xd6>V\xc0\xbb\x8fp\x16#Z\x0b\x81\xeb\x16'
+SECRET_KEY = 'replace_this'
 DEBUG = True
 DEFAULT_HOST = '0.0.0.0'    # use public IPs
 DEFAULT_PORT = 5000
