8bae4b7a20ea053fae554969d8bc00bfe9e18ef0
krassowski <krassowski.michal@gmail.com>
2016-09-02 12:23:17 -0400

    Create a close button in tooltips

--- website/static/needleplot.js
+++ website/static/needleplot.js
@@ -1,6 +1,7 @@
 var Tooltip = function()
 {
     var tooltip
+    var tooltip_content
 
     var _template = function(d)
     {
@@ -17,12 +18,22 @@ var Tooltip = function()
                 .style('opacity', 0)
                 .style('pointer-events', 'none')
 
+            tooltip_content = tooltip
+                .append('div')
+                .attr('class', 'content')
+
             if(custom_template !== undefined)
             {
                 _template = custom_template
             }
             d3.select('body')
                 .on('click.' + id, publicSpace.unstick)
+
+            // create a close button
+            tooltip.append('a')
+                .attr('class', 'btn btn-xs btn-danger close')
+                .html('x')
+                .on('mouseup', publicSpace.unstick)
         },
         show: function(d)
         {
@@ -32,7 +43,8 @@ var Tooltip = function()
             tooltip.transition()
                 .duration(50)
                 .style('opacity', 1)
-            tooltip.html(_template(d))
+            tooltip_content.html(_template(d))
+
             publicSpace.move()
         },
         hide: function(v)
@@ -73,6 +85,11 @@ var Tooltip = function()
                 .on('mouseover', publicSpace.show)
                 .on('mousemove', publicSpace.move)
                 .on('mouseout', publicSpace.hide)
+
+            // do not close the tooltip whene selecting
+            tooltip
+                .on('click', function(){ d3.event.stopPropagation() })
+
         }
     }
 
