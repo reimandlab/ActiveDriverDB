75008c5117b016fed11c2ba311471900c9eaee0c
krassowski <krassowski.michal@gmail.com>
2016-09-04 17:53:12 +0200

    Activate useful plugins of editor, configure it.

--- website/static/cms_editor.js
+++ website/static/cms_editor.js
@@ -4,5 +4,18 @@ tinymce.init({
     content_css : [
         '/static/min/page.css',
         '/static/thirdparty/bootstrap/css/bootstrap.min.css',
+    ],
+    height: 160,
+    browser_spellcheck: true,
+    plugins : 'anchor advlist autolink link image lists charmap print preview fullscreen table imagetools textcolor colorpicker searchreplace',
+    table_default_attributes: {
+        class: 'table table-bordered table-hover'
+    },
+    table_class_list: [
+        {title: 'Default', value: 'table table-bordered table-hover'},
+        {title: 'Striped', value: 'table table-bordered table-hover table-striped'},
+        {title: 'Without borders', value: 'table table-hover table-striped'},
+        {title: 'Striped, without borders', value: 'table table-hover table-striped'},
     ]
+
 })
