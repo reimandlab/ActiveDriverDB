7456447a2edeb0666f18b4243fa2bfd15d613aef
krassowski <krassowski.michal@gmail.com>
2016-09-05 13:12:47 +0200

    Move assets definitions into seperate file
    so there won't be a need to constantly edit app.py core file

--- website/app.py
+++ website/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask_assets import Bundle
 from flask_assets import Environment
 from flask_login import LoginManager
 from database import db
+from assets import bundles
 
 
 app = Flask(__name__)
@@ -20,73 +20,9 @@ login_manager.init_app(app)
 
 
 """
-Define assets
+Register assets
 """
 assets = Environment(app)
-
-
-def css_bundle(name, *args):
-    return Bundle(
-        *args,
-        filters='cssutils',
-        output='min/' + name + '.css'
-    )
-
-
-bundles = {
-    'js_search': Bundle(
-        'search.js',
-        filters='rjsmin',
-        output='min/search.js'
-    ),
-    'js_protein_view': Bundle(
-        'common.js',
-        'needleplot.js',
-        'filters.js',
-        'tracks.js',
-        filters='rjsmin',
-        output='min/proteinView.js'
-    ),
-    'js_network_view': Bundle(
-        'common.js',
-        'orbits.js',
-        'network.js',
-        'filters.js',
-        filters='rjsmin',
-        output='min/networkView.js'
-    ),
-    'js_cms_editor': Bundle(
-        'cms_editor.js',
-        output='min/editor.js'
-    ),
-    'css_common': css_bundle(
-        'style',
-        'sass/style.css'
-    ),
-    'css_network': css_bundle(
-        'network',
-        'sass/network.css',
-        'sass/filters.css'
-    ),
-    'css_protein': css_bundle(
-        'protein',
-        'sass/protein.css',
-        'sass/tracks.css',
-        'sass/filters.css'
-    ),
-    'css_search': css_bundle(
-        'search',
-        'sass/search.css'
-    ),
-    'css_page': css_bundle(
-        'page',
-        'sass/page.css'
-    ),
-    'css_admin': css_bundle(
-        'admin',
-        'sass/admin.css'
-    )
-}
 
 for name, bundle in bundles.items():
     assets.register(name, bundle)
