3220b356297ec5fe61888a906543d0ee993f9f31
krassowski <krassowski.michal@gmail.com>
2016-09-05 21:21:38 +0200

    Add more tests to database

--- website/tests/test_database.py
+++ website/tests/test_database.py
@@ -2,18 +2,22 @@ import database
 
 
 def test_encode_csv():
-    attributes = (
+    test_data = (
         # strand, ref, alt, cdna_pos, exon, protein_id, is_ptm
-        '+', 'R', 'H', 204, 'exon1', 123, False
+        (('+', 'R', 'H', 204, 'exon1', 123, False), '+RH0cc:exon1:7b'),
+        (('-', 'R', 'H', 204, 'exon1', 123, True), '-RH1cc:exon1:7b'),
     )
-    result = database.encode_csv(*attributes)
-    assert result == '+RH0cc:exon1:7b'
+    for attributes, correct_result in test_data:
+        result = database.encode_csv(*attributes)
+        assert result == correct_result
 
 
 def test_decode_csv():
-    encoded_csv = '+RH0cc:exon1:7b'
-    result = database.decode_csv(encoded_csv)
-    assert result == dict(zip(
-        ('strand', 'ref', 'alt', 'pos', 'cdna_pos', 'exon', 'protein_id', 'is_ptm'),
-        ('+', 'R', 'H', 68, 204, 'exon1', 123, False)
-    ))
+    keys = ('strand', 'ref', 'alt', 'pos', 'cdna_pos', 'exon', 'protein_id', 'is_ptm')
+    test_data = (
+        ('+RH0cc:exon1:7b', ('+', 'R', 'H', 68, 204, 'exon1', 123, False)),
+        ('-RH1cc:exon1:7b', ('-', 'R', 'H', 68, 204, 'exon1', 123, True)),
+    )
+    for encoded_csv, correct_result in test_data:
+        result = database.decode_csv(encoded_csv)
+        assert result == dict(zip(keys, correct_result))
