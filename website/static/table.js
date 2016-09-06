b10f2704bc819ba9238e04c1462662cf02813be3
krassowski <krassowski.michal@gmail.com>
2016-09-06 11:37:44 +0200

    Correct column numbers after 'mutation type' deletion

--- website/static/table.js
+++ website/static/table.js
@@ -4,9 +4,9 @@ var MutationTable = function ()
 
     function detailFormatter(index, row, element)
     {
-        html = 'Impact: ' + row[5] + '<br>'
-        html += 'Sites affected: ' + row[6] + '<br>'
-        html += 'Closest affected site(s): <div>' + row[7] + '</div>'
+        html = 'Impact: ' + row[4] + '<br>'
+        html += 'Sites affected: ' + row[5] + '<br>'
+        html += 'Closest affected site(s): <div>' + row[6] + '</div>'
         return html
     }
 
