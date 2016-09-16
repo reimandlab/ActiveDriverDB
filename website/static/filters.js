5e3a802e98570438a8fd1cdef53c2fb7b2711cbc
krassowski <krassowski.michal@gmail.com>
2016-09-16 11:29:09 +0200

    Don't send request if multielect not changed & require at least one selection

--- website/static/filters.js
+++ website/static/filters.js
@@ -9,6 +9,19 @@ var Filters = (function ()
         form.submit()
     }
 
+    function checkEquality(obj1, obj2)
+    {
+        if(obj1.length != obj2.length)
+            return false
+
+        for(var i = 0; i < obj1.length; i++)
+        {
+            if(obj1[i] != obj2[i])
+                return false
+        }
+        return true
+    }
+
 	var publicSpace = {
 		init: function(filter_box)
 		{
@@ -28,7 +41,46 @@ var Filters = (function ()
 
             // initialize multiselect fields
             $('.multiselect').multiselect({
-                onDropdownHidden: update,
+                onDropdownShow: function(event)
+                {
+                    var target = $(event.target)
+
+                    var selected = target.find('li.active')
+
+                    target.data('prevoiously_selected', selected)
+                },
+                onDropdownHide: function(event)
+                {
+                    var target = $(event.target)
+
+                    // selected counts also 'Select all' option
+                    var selected = target.find('li.active')
+
+                    // if there was not a change do not consider this action
+                    if(checkEquality(selected, target.data('prevoiously_selected')))
+                    {
+                        return true
+                    }
+                    if (selected.length < 1)
+                    {
+                        target.parent().popover({
+                            content: 'You should select at least one option',
+                            placement: 'top',
+                            trigger: 'manual'
+                        }).popover('show')
+                        return false
+                    }
+                    else {
+                        target.parent().popover('hide')
+                        update()
+                    }
+
+                },
+                onChange: function(option, checked)
+                {
+                    if(checked)
+                        $(option).parent().parent().popover('hide')
+                },
                 includeSelectAllOption: true,
                 dropRight: true
             })
