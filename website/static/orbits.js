260f6af94f2fb78dae639d6fba68feb10564c670
krassowski <krassowski.michal@gmail.com>
2016-09-23 22:16:07 +0200

    Rewritten orbits code to be more object-oriented, and easier to maintain

--- website/static/orbits.js
+++ website/static/orbits.js
@@ -36,13 +36,16 @@ var Orbits = function ()
     }
 
     // warning: using constructor pattern, not module
-    function Orbit(number)
+    function Orbit(number, initial_outer_radius)
     {
         this.number = number
         this.nodes_count = 0
 
-        this.radius = null
-        this.width = null   // how much space takes the biggest node on this orbit
+        this.outer_radius = initial_outer_radius
+        this.radius = null  // inner radius in pixels
+        this.width = 0   // how much space takes the biggest node on this orbit / 2
+
+        this.space_available = perimeter(this.outer_radius)    // space available on outer belt
 
         this.addNode = function(node)
         {
@@ -50,11 +53,29 @@ var Orbits = function ()
             this.nodes_count += 1
         }
 
-        this.setDimensions = function(R, length_extend)
+        this.setDimensions = function(outer_radius)
         {
-            this.radius = R - length_extend
-            this.width = length_extend
+            this.radius = outer_radius - this.width
         }
+
+        this.getSpaceRequiredForNewNode = function()
+        {
+            var angle = 2 * Math.asin(this.width / this.outer_radius)
+            return this.outer_radius * angle
+        }
+
+        this.recalculateSpace= function(old_perimeter)
+        {
+            var percent_available = this.space_available / old_perimeter
+            this.space_available = perimeter(this.outer_radius) * percent_available
+        }
+    }
+
+    function newOrbit(initial_outer_radius)
+    {
+        var orbit = new Orbit(orbits.length, initial_outer_radius)
+        orbits.push(orbit)
+        return orbit
     }
 
     function calculateOrbits()
@@ -62,57 +83,48 @@ var Orbits = function ()
         // radius of the first orbit, depends of the size of central protein
         var base_length = central_node.r * config.first_ring_scale
 
-        orbits.push(new Orbit(0))
-        var length_extend = 0  // how much the radius will extend on the current orbit
-
-        var R = base_length + length_extend * 2
-        var outer_belt_perimeter = perimeter(R)
-        var available_space_on_outer_belt = outer_belt_perimeter
+        var orbit = newOrbit(base_length)
 
         for(var i = 0; i < nodes.length; i++)
         {
             var node = nodes[i]
+            var node_width = node.r + config.stroke
 
-            if(node.r > length_extend)
+            if(node_width > orbit.width)
             {
-                // the outer belt will be larger - let's rescale the outer belt
-                length_extend = node.r + config.stroke
-                R = base_length + length_extend * 2
+                // recalculate width, radius & available space
+                var old_perimeter = perimeter(orbit.outer_radius)
 
-                // recalculate perimeter, width & available space
-                var new_outer_belt_perimeter = perimeter(R)
-                percent_available = available_space_on_outer_belt / outer_belt_perimeter
-                available_space_on_outer_belt = new_outer_belt_perimeter * percent_available
-                outer_belt_perimeter = new_outer_belt_perimeter
+                // the outer belt will be larger - let's rescale the outer belt
+                orbit.width = node_width
+                orbit.outer_radius = base_length + orbit.width * 2
+                orbit.recalculateSpace(old_perimeter)
 
             }
-            var angle = 2 * Math.asin(length_extend / R)
-            var l = R * angle
+            var space_for_node = orbit.getSpaceRequiredForNewNode()
             // if it does not fit, lets move the next orbit, reset values
-            if(available_space_on_outer_belt < l)
+            if(orbit.space_available < space_for_node)
             {
                 // save the orbit which is full
-                orbits[orbits.length - 1].setDimensions(R, length_extend)
+                orbit.setDimensions(orbit.outer_radius)
+
                 // create new orbit
-                base_length = R + config.spacing
-                R = base_length + length_extend * 2
-                outer_belt_perimeter = perimeter(R)
-                available_space_on_outer_belt = outer_belt_perimeter
-                length_extend = node.r + config.stroke
-                angle = 2 * Math.asin(length_extend / R)
-                l = R * angle
+                base_length = orbit.outer_radius + config.spacing
+
                 // move to the new orbit
-                orbits.push(new Orbit(orbits.length))
+                orbit = newOrbit(base_length + node_width * 2)
+                orbit.width = node_width
+                space_for_node = orbit.getSpaceRequiredForNewNode()
             }
 
             // finaly since it has to fit to the orbit right now, place it on the current orbit
-            orbits[orbits.length - 1].addNode(node)
-            available_space_on_outer_belt -= l
-            if(available_space_on_outer_belt < 0)
-                available_space_on_outer_belt = 0
+            orbit.addNode(node)
+            orbit.space_available -= space_for_node
+
+            // (orbit.space_available >= 0) should always be true
 
         }
-        orbits[orbits.length - 1].setDimensions(R, length_extend)
+        orbit.setDimensions(orbit.outer_radius)
     }
 
     function getSlots()
