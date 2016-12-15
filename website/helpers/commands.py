72edff5f9e8982b76a8e6f00a47ca72477df2fda
krassowski <krassowski.michal@gmail.com>
2016-12-16 00:58:29 +0100

    Refactor database import management to form of nested commands

---
+++ website/helpers/commands.py
@@ -0,0 +1,95 @@
+from types import FunctionType
+
+
+def argument_parameters(*args, **kwargs):
+    return (args, kwargs)
+
+
+def static(func):
+    def wrapped(self, *args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapped
+
+
+def command(func):
+    method = func
+    method.is_command = True
+    return method
+
+
+def argument(func):
+    func.is_argument = True
+    return func
+
+
+class Register(type):
+
+    def __init__(cls, name, bases, newattrs):
+
+        super().__init__(name, bases, newattrs)
+
+        if not hasattr(cls, 'registry'):
+            cls.registry = set()
+
+        cls.registry.add(cls)
+        cls.registry -= set(bases)
+
+    def __iter__(cls):
+        return iter(cls.registry)
+
+
+class CommandTarget(metaclass=Register):
+
+    @classmethod
+    def name(cls):
+        camel_name = cls.__name__
+        underscore_name = ''
+        was_up = True
+        for c in camel_name:
+            is_up = c.isupper()
+            if is_up and not was_up:
+                underscore_name += '_'
+            was_up = is_up
+            underscore_name += c.lower()
+        return underscore_name
+
+    @classmethod
+    def get_methods(cls):
+        return [
+            (name, getattr(cls, name))
+            for name in dir(cls)
+            if isinstance(getattr(cls, name), FunctionType)
+        ]
+
+    @classmethod
+    def get_arguments(cls):
+        return [
+            value()
+            for key, value in cls.get_methods()
+            if value and value.__dict__.get('is_argument', False)
+        ]
+
+    @classmethod
+    def supports(cls, command_name):
+        members = dict(cls.get_methods())
+        function = members.get(command_name, None)
+        return function and function.__dict__.get('is_command', False)
+
+
+def create_command_subparsers(command_parsers):
+    for command, command_parser in command_parsers.items():
+        subparsers = command_parser.add_subparsers(help='sub-commands')
+
+        for handler in CommandTarget:
+            if handler.supports(command):
+
+                subparser = subparsers.add_parser(
+                    handler.name(),
+                    help=handler.description.format(command=command)
+                )
+                command_func = getattr(handler, command)
+                subparser.set_defaults(func=command_func)
+
+                for arg_parameters in handler.get_arguments():
+                    args, kwargs = arg_parameters
+                    subparser.add_argument(*args, **kwargs)
