41a97e6e4b1cee188f6dea4badf7e9dc10d7e50a
krassowski <krassowski.michal@gmail.com>
2017-04-15 13:11:56 -0400

    Add more plugins to cms tinymce editor

--- website/static/cms_editor.js
+++ website/static/cms_editor.js
@@ -7,15 +7,23 @@ tinymce.init({
     ],
     height: 160,
     browser_spellcheck: true,
-    plugins : 'anchor advlist autolink link image lists charmap print preview fullscreen table imagetools textcolor colorpicker searchreplace',
+    plugins : 'anchor advlist autolink link image lists charmap print preview fullscreen table imagetools textcolor colorpicker searchreplace code codesample autoresize',
+    toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | codesample',
     table_default_attributes: {
         class: 'table table-bordered table-hover'
     },
+    autoresize_max_height: 600,
     table_class_list: [
         {title: 'Default', value: 'table table-bordered table-hover'},
         {title: 'Striped', value: 'table table-bordered table-hover table-striped'},
         {title: 'Without borders', value: 'table table-hover table-striped'},
         {title: 'Striped, without borders', value: 'table table-hover table-striped'}
-    ]
-
+    ],
+    codesample_languages: [
+        {text: 'HTML/XML', value: 'markup'},
+        {text: 'JavaScript', value: 'javascript'},
+        {text: 'CSS', value: 'css'},
+        {text: 'Python', value: 'python'},
+        {text: 'Bash', value: 'bash'},
+    ],
 })
