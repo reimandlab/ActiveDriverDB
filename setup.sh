42e614fc62d870b79948c0e09bf91e488ea447ba
krassowski <krassowski.michal@gmail.com>
2018-01-05 16:14:05 -0100

    Use newer BerkleyDB version

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 # Install Berkley DB
 mkdir downloads
 cd downloads
-wget -N http://download.oracle.com/berkeley-db/db-6.2.23.NC.tar.gz
-tar -xzf db-6.2.23.NC.tar.gz
-cd db-6.2.23.NC/build_unix
+wget -N http://download.oracle.com/berkeley-db/db-6.2.32.NC.tar.gz -O db.NC.tar.gz
+tar -xzf db.NC.tar.gz
+cd db.NC/build_unix
 sudo ../dist/configure
 sudo make
 sudo make install
