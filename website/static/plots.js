9862643626be88bd17eacb20ec962b8c1b20625f
krassowski <krassowski.michal@gmail.com>
2018-02-11 18:07:44 -0100

    Make plot downloads format = 'svg' & keep size

--- website/static/plots.js
+++ website/static/plots.js
@@ -17,4 +17,34 @@
             Plotly.Plots.resize(plot);
     })
     window.onresize = resize_all
+
+})()
+
+plot = (function () {
+
+    function save_svg(gd) {
+        console.log($(gd).width())
+        Plotly.downloadImage(gd, {
+            format: 'svg',
+            width: $(gd).width(),
+            height: $(gd).height(),
+            filename: gd.id
+        })
+    }
+
+    function plot(element_id, data, layout){
+
+        var COMMON_PLOT_OPTIONS = {
+            displaylogo: false,
+            modeBarButtonsToRemove: ['toImage'],
+            modeBarButtonsToAdd: [{
+                name: 'Save as SVG',
+                icon: Plotly.Icons.camera,
+                click: save_svg
+            }]
+        }
+        return Plotly.newPlot(element_id, data, layout, COMMON_PLOT_OPTIONS)
+    }
+
+    return plot
 })()
