cf6d639d6537e310e5e19a2eaac5a532eb42ce99
krassowski <krassowski.michal@gmail.com>
2018-03-02 14:16:56 +0100

    Add biogrid network for hotnet2

--- website/analyses/hotnet2.py
+++ website/analyses/hotnet2.py
@@ -1,12 +1,16 @@
 from functools import partial
 from itertools import chain
 from pathlib import Path
-from subprocess import Popen
+from subprocess import Popen, check_output
 from tempfile import NamedTemporaryFile
 from typing import List
 
+from pandas import read_table
+
 from analyses import active_driver
 from analyses.active_driver import ActiveDriverResult
+from database import db
+from models import Pathway, Gene
 
 
 def python(module_name, *args, python_path='python', working_dir=Path('.'), key_prefix='--', **kwargs) -> List:
@@ -31,47 +35,103 @@ def python(module_name, *args, python_path='python', working_dir=Path('.'), key_
 
 
 def run_python(*args, **kwargs):
-    return Popen(python(*args, **kwargs)).wait()
+    cmd = python(*args, **kwargs)
+    return check_output(cmd)
 
 
-def create_network_files(hotnet, networks_dir, networks, num_network_permutations=100, num_cores=-1):
+class HotNet:
 
-    for name, beta in networks.items():
-        hotnet(
-            'makeNetworkFiles',
-            edgelist_file=f'{networks_dir}/{name}/{name}_edge_list',
-            gene_index_file=f'{networks_dir}/{name}/{name}_index_gene',
-            network_name=name,
-            prefix=name,
-            beta=beta,
-            output_dir=f'{networks_dir}/{name}',
-            num_permutations=num_network_permutations,
-            cores=num_cores
+    def __init__(self, interpreter, path):
+        path = Path(path).absolute()
+        self.networks_dir = path / 'paper/data/networks'
+        self.command = partial(run_python, python_path=interpreter, working_dir=path)
+
+    def edge_list(self, name):
+        return self.networks_dir / f'{name}/{name}_edge_list'
+
+    def gene_list(self, name):
+        return self.networks_dir / f'{name}/{name}_index_gene'
+
+    def beta_file(self, name):
+        return self.networks_dir / f'{name}/{name}_beta'
+
+    def permutations_file(self, name, beta):
+        return self.networks_dir / f'{name}/{name}_ppr_{beta}.h5'
+
+    def create_network_files(self, networks, num_network_permutations=100, num_cores=-1):
+
+        for name, beta in networks.items():
+            self.command(
+                'makeNetworkFiles',
+                edgelist_file=self.edge_list(name),
+                gene_index_file=self.gene_list(name),
+                network_name=name,
+                prefix=name,
+                beta=beta,
+                output_dir=f'{self.networks_dir}/{name}',
+                num_permutations=num_network_permutations,
+                cores=num_cores
+            )
+
+    def create_network(self, name, data_path, filter_by_gmt_pathways=True):
+        network_dir = self.networks_dir / name
+        network_dir.mkdir(exist_ok=True)
+
+        network = read_table(data_path)
+
+        if filter_by_gmt_pathways:
+            all_genes_in_pathways = {name for name, in db.session.query(Gene.name).join(Pathway.genes)}
+            network = network.ix[
+                [
+                    edge.Index
+                    for edge in network.itertuples()
+                    if edge.protein1 in all_genes_in_pathways and edge.protein2 in all_genes_in_pathways
+                ]
+            ]
+
+        with NamedTemporaryFile('w') as network_file:
+
+            network[['protein1', 'protein2']].to_csv(network_file, sep='\t', index=False)
+
+            self.command(
+                'scripts/createNetwork',
+                network_file=network_file.name,
+                edgelist_file=self.edge_list(name),
+                gene_index_file=self.gene_list(name),
+            )
+
+        beta_file = self.beta_file(name)
+        self.command(
+            'scripts/chooseBeta',
+            edge_list_file=self.edge_list(name),
+            output_file=beta_file
         )
+
+        return beta_file
+
+
+HOTNET_PUBLICATION_NETWORKS = {
+    'hint+hi2012': 0.4,
+    'irefindex9': 0.45,
+    'multinet': 0.5,
+}
 
 
 def run_on_active_driver_results(
-    active_driver_results: ActiveDriverResult, hotnet_path, interpreter='python',
+    active_driver_results: ActiveDriverResult, hotnet_path,
+    interpreter='python', networks=HOTNET_PUBLICATION_NETWORKS,
     num_heat_permutations=1000, num_network_permutations=100, num_cores=-1
 ):
     hotnet_path = Path(hotnet_path)
 
-    networks_dir = hotnet_path / 'paper/data/networks'
-
-    networks = {
-        'hint+hi2012': 0.4,
-        'irefindex9': 0.45,
-        'multinet': 0.5
-    }
-
-    hotnet = partial(run_python, python_path=interpreter, working_dir=hotnet_path)
+    hotnet = HotNet(interpreter, hotnet_path)
 
     if any(
-        not (networks_dir / f'{name}/{name}_ppr_{beta}.h5').exists()
+        not hotnet.permutations_file(name, beta).exists()
         for name, beta in networks.items()
     ):
         print('Creating network files...')
-        create_network_files(hotnet, networks_dir, networks, num_network_permutations, num_cores)
+        hotnet.create_network_files(networks, num_network_permutations, num_cores)
 
     df = active_driver_results['all_gene_based_fdr']
     df = df.set_index('gene')['p']
@@ -82,7 +142,7 @@ def run_on_active_driver_results(
         df.to_csv(input_heat_file.name, sep='\t')
 
         # convert the heat file into HotNet json file
-        results = hotnet(
+        results = hotnet.command(
             'makeHeatFile',
             'scores',
             heat_file=input_heat_file.name,
@@ -90,7 +150,7 @@ def run_on_active_driver_results(
         )
 
         # run HotNet
-        hotnet(
+        hotnet.command(
             'HotNet2',
             network_files=[
                 f'{networks_dir}/{name}/{name}_ppr_{beta}.h5'
@@ -113,3 +173,20 @@ def run_all(site_type: str, **kwargs):
     for analysis in [active_driver.pan_cancer_analysis, active_driver.clinvar_analysis]:
         result = analysis(site_type)
         run_on_active_driver_results(result, **kwargs)
+
+
+def run_all_from_biogrid(site_type: str, hotnet_path, interpreter, **kwargs):
+    hot_net = HotNet(interpreter, hotnet_path)
+
+    beta_file = hot_net.beta_file('biogrid_hc')
+
+    if not beta_file.exists():
+        hot_net.create_network('biogrid_hc', 'data/networks/PPI_network_BioGrid_HC.txt')
+
+    with open(beta_file) as f:
+        beta = float(f.read().strip())
+
+    run_all(
+        site_type, interpreter=interpreter, hotnet_path=hotnet_path,
+        networks={'biogrid_hc': beta}, **kwargs
+    )
