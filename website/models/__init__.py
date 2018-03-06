e0d01044965d8063b3600ddefe45d3a700e8ad91
krassowski <krassowski.michal@gmail.com>
2018-03-06 13:25:16 +0100

    Add simple repr for all models to speed up generic debuging

--- website/models/__init__.py
+++ website/models/__init__.py
@@ -1,3 +1,5 @@
+from warnings import warn
+
 from database import db
 from sqlalchemy.ext.declarative import declared_attr
 
@@ -13,6 +15,27 @@ class Model(db.Model):
     @declared_attr
     def id(cls):
         return db.Column('id', db.Integer, primary_key=True)
+
+    def __repr__(self):
+
+        try:
+            properties = {}
+
+            for k, v in self.__dict__.items():
+                if k.startswith('_'):
+                    continue
+                v = repr(v)
+                if len(v) > 50:
+                    v = v[:25] + '...' + v[-25:]
+                properties[k] = v
+
+            description = ', '.join(f'{k}={v}' for k, v in properties.items())
+
+            return f'<{self.__class__.__name__}: {description}>'
+
+        except Exception as e:
+            warn(f'Could not generate repr for: {self} ({e}')
+            return super().__repr__()
 
 
 from .bio import *
