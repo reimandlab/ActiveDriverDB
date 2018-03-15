dad526359e2963557192326060268b7f04b7a1eb
krassowski <krassowski.michal@gmail.com>
2018-03-15 11:16:48 +0100

    Add gains and losses plot (MIMP)

--- website/stats/plots/__init__.py
+++ website/stats/plots/__init__.py
@@ -7,7 +7,7 @@ from helpers.plots import bar_plot
 from models import Plot, Site, Protein
 from stats.plots import (
     ptm_variability, proteins_variability, most_mutated_sites, active_driver, ptm_mutations,
-    gene_ontology, motifs,
+    gene_ontology, motifs, mimp,
 )
 from stats.plots.common import site_types_names, any_site_type
 from ..store import cases, CountStore
@@ -39,7 +39,8 @@ plot_modules = [
     active_driver,
     ptm_mutations,
     gene_ontology,
-    motifs
+    motifs,
+    mimp
 ]
 
 
