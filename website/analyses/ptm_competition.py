5d9ab6b8bb5acad6aa434940a315f6fec701a063
krassowski <krassowski.michal@gmail.com>
2018-06-05 10:56:00 -0400

    Add sites in proximity analyzes

--- website/analyses/ptm_competition.py
+++ website/analyses/ptm_competition.py
@@ -5,7 +5,7 @@ from pandas import DataFrame
 from sqlalchemy import and_
 from sqlalchemy.orm import aliased
 
-from models import SiteType, Site, Protein, Mutation, source_manager, InheritedMutation, db
+from models import SiteType, Site, Protein, Mutation, source_manager, InheritedMutation, db, MC3Mutation
 
 
 def site_type_filter_from_str(query, site=Site):
@@ -122,3 +122,84 @@ def glycosylation_muts(only_preferred=True):
     df = DataFrame(data=results).transpose()
     print(df)
     return df
+
+
+def ptm_sites_in_proximity(type_1: str, type_2: str, distance: int=7, only_preferred=True) -> int:
+    site_1 = Site
+    site_2 = aliased(Site)
+
+    site_and_type = [(site_1, type_1), (site_2, type_2)]
+
+    sites = site_1.query.join(
+        site_2,
+        and_(
+            site_1.position.between(
+                site_2.position - distance,
+                site_2.position + distance
+            ),
+            site_1.protein_id == site_2.protein_id
+        )
+    )
+
+    for site, type_name in site_and_type:
+        site_filter = site_type_filter_from_str(type_name, site=site)
+        if site_filter is None:
+            continue
+        sites = sites.filter(site_filter)
+
+    if only_preferred:
+        # no need to repeat as we already joined on protein_id
+        sites = sites.join(Site.protein).filter(Protein.is_preferred_isoform)
+
+    return sites, [site_1, site_2]
+
+
+def count_ptm_sites_in_proximity(*args, **kwargs):
+    sites, _ = ptm_sites_in_proximity(*args, **kwargs)
+    return sites.count()
+
+
+def mutated_ptm_sites_in_proximity(mutation_source, type_1: str, type_2: str, mutation_filter=True, distance: int=7, only_preferred=True) -> int:
+    sites, (site_1, site_2) = ptm_sites_in_proximity(type_1, type_2, distance, only_preferred)
+    for site in (site_1, site_2):
+        sites = sites.filter(
+            site.affected_by_mutations.any(
+                and_(
+                    Mutation.in_sources(mutation_source),
+                    mutation_filter
+                )
+            )
+        )
+    return sites
+
+
+def mutated_sites_overlap(site_type_name, cases, only_preferred=True, additional_types={'not glycosylation'}):
+    results = {}
+    site_type_names = [name for (name, ) in db.session.query(SiteType.name)]
+    for source, additional_filter in cases:
+        result = {}
+        for type_query in chain(site_type_names, additional_types):
+            sites = mutated_ptm_sites_in_proximity(
+                source, site_type_name, type_query,
+                only_preferred=only_preferred,
+                mutation_filter=additional_filter
+            )
+            sites_count = sites.count()
+            result[type_query] = sites_count
+        results[source.name] = result
+    df = DataFrame(data=results).transpose()
+    return df
+
+
+def total_mutated_sites_overlap(site_type_name, cases, other_type_names, only_preferred=True):
+    results = {}
+    all_sites = set()
+    for source, additional_filter in cases:
+        for type_query in other_type_names:
+            sites = mutated_ptm_sites_in_proximity(
+                source, site_type_name, type_query,
+                only_preferred=only_preferred,
+                mutation_filter=additional_filter
+            )
+            all_sites.update(sites)
+    return all_sites
