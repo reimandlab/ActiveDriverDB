aaec3d3792ca700439bbf367266d46f55f135cee
krassowski <krassowski.michal@gmail.com>
2019-01-11 19:12:46 +0000

    Filter out third-party deprecation warnings,
    which might be the cause of tests failing on Travis

--- website/views/__init__.py
+++ website/views/__init__.py
@@ -1,3 +1,5 @@
+import warnings
+
 from .protein import ProteinView
 from .gene import GeneView
 from .search import SearchView
@@ -8,6 +10,13 @@ from .chromosome import ChromosomeView
 from .short_url import ShortAddress
 from .pathway import PathwaysView
 from .mutation import MutationView
+
+
+# see https://github.com/teracyhq/flask-classful/issues/94
+warnings.filterwarnings(
+    'ignore',
+    message='inspect.getargspec() is deprecated, use inspect.signature() or inspect.getfullargspec()'
+)
 
 
 views = [
