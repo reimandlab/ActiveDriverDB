a81a04668f28aa5c82a1b49b264d2e8c4847a70d
krassowski <krassowski.michal@gmail.com>
2019-04-28 18:09:50 -0400

    Stick to the old filters structure format

--- website/helpers/filters.py
+++ website/helpers/filters.py
@@ -696,7 +696,11 @@ class FilterManager:
         if args.get('clear_filters'):
             filters_list = self._parse_fallback_query({})
         elif args.get('fallback'):
-            filters_list = self._parse_fallback_query(args.to_dict(flat=False))
+            data = {
+                key: value[0] if isinstance(value, list) and len(value) == 1 else value
+                for key, value in args.to_dict(flat=False).items()
+            }
+            filters_list = self._parse_fallback_query(data)
         else:
             filters_list = self._parse_string(
                 args.get('filters', '')
