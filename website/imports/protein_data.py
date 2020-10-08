1259137ce8a352c7356f1ad76e78d0707c6b5e0b
krassowski <krassowski.michal@gmail.com>
2020-10-07 23:22:32 -0400

    Fix progress bar

--- website/imports/protein_data.py
+++ website/imports/protein_data.py
@@ -1340,7 +1340,7 @@ def drugbank(path='data/full database.xml'):
 
     unique_gene_targets = drug_targets.drop_duplicates(subset=['drug_name', 'gene_name'])
 
-    for _, record in tqdm(unique_gene_targets.iterrows(), total=unique_gene_targets):
+    for _, record in tqdm(unique_gene_targets.iterrows(), total=len(unique_gene_targets)):
         # drug_id, gene_name, drug_name, drug_groups, drug_type_name = data
         target_gene = Gene.query.filter_by(name=record.gene_name).first()
 
