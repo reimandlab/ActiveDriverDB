fb97f16ee6069850001d2666c5525a090008ba03
krassowski <krassowski.michal@gmail.com>
2021-03-06 21:00:57 +0000

    Add missing colons

--- website/example_database.sql
+++ website/example_database.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE db_bio;
 CREATE DATABASE db_cms;
 
-CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass'
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass';
 
-GRANT ALL PRIVILEGES ON db_bio.* TO 'user'@'localhost'
-GRANT ALL PRIVILEGES ON db_cms.* TO 'user'@'localhost'
-GRANT INSERT, DELETE, CREATE ROUTINE, ALTER ROUTINE, EXECUTE ON mysql.* TO 'user'@'localhost'
+GRANT ALL PRIVILEGES ON db_bio.* TO 'user'@'localhost';
+GRANT ALL PRIVILEGES ON db_cms.* TO 'user'@'localhost';
+GRANT INSERT, DELETE, CREATE ROUTINE, ALTER ROUTINE, EXECUTE ON mysql.* TO 'user'@'localhost';
